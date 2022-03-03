8bda706f88d953f01b6736fd5318945eac09856d
ParkSangSun <appleasz1028102@gmail.com>
2022-03-03 13:08:28 +0900

    19. Modifier로 컴포저블 수정하기

--- app/src/main/java/com/pss/jetpackcomposestudy/MainActivity.kt
+++ app/src/main/java/com/pss/jetpackcomposestudy/MainActivity.kt
@@ -3,32 +3,39 @@ package com.pss.jetpackcomposestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.pss.jetpackcomposestudy.ui.theme.JetpackComposeStudyTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       /* //아래와 그 아래 setContent 2개 모두 같은 의미
-        setContent(content = {
-            Greeting("Android")
-        })*/
+        /* //아래와 그 아래 setContent 2개 모두 같은 의미
+         setContent(content = {
+             Greeting("Android")
+         })*/
         setContent {
-            GreetingButton()
+            GreetingText("world")
         }
     }
 }
 
 @Composable
 fun GreetingText(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier
+            .fillMaxWidth(0.8f)
+    )
 }
 
 @Composable
-fun GreetingButton(){
+fun GreetingButton() {
     Button(onClick = {}) {
         GreetingText(name = "button")
         GreetingText(name = "버튼")
@@ -38,7 +45,5 @@ fun GreetingButton(){
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    JetpackComposeStudyTheme {
-        GreetingButton()
-    }
+    GreetingText("world")
 }
