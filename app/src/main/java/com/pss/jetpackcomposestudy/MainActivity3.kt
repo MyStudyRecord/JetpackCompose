eca30a867cc88914ce5cfe68345c79c7f074c0b0
ParkSangSun <appleasz1028102@gmail.com>
2022-03-15 19:08:42 +0900

    39. 재구성

--- app/src/main/java/com/pss/jetpackcomposestudy/MainActivity3.kt
+++ app/src/main/java/com/pss/jetpackcomposestudy/MainActivity3.kt
@@ -3,17 +3,20 @@ package com.pss.jetpackcomposestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.pss.jetpackcomposestudy.ui.theme.JetpackComposeStudyTheme
 
-val namesList : List<String> = listOf("Park","Sang","Sun")
+val namesList: ArrayList<String> = arrayListOf("Park", "Sang", "Sun", "Kim")
 
 class MainActivity3 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,14 +29,25 @@ class MainActivity3 : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        style = MaterialTheme.typography.h3
+    )
 }
 
 @Composable
-fun GreetingList(names : List<String>) {
-    Column() {
-        for (name in names){
+fun GreetingList(names: List<String>) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        for (name in names) {
             Greeting(name = name)
+        }
+
+        Button(onClick = { namesList.add("new name") }) {
+            Text("Add new name")
         }
     }
 }
