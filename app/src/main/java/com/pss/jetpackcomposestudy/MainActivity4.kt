494fc2528a826b46b9c68165b4d356f4a9c09cca
ParkSangSun <appleasz1028102@gmail.com>
2022-04-28 20:51:01 +0900

    49. 카드 프로필 아이콘 생성

--- app/src/main/java/com/pss/jetpackcomposestudy/MainActivity4.kt
+++ app/src/main/java/com/pss/jetpackcomposestudy/MainActivity4.kt
@@ -3,16 +3,20 @@ package com.pss.jetpackcomposestudy
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pss.jetpackcomposestudy.ui.theme.JetpackComposeStudyTheme
@@ -37,24 +41,37 @@ fun Main4Screen() {
 }
 
 @Composable
+fun ProfileCard(){
+    Card(modifier = Modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .wrapContentHeight(align = Alignment.Top),
+        elevation = 8.dp) {
+        Row(modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start) {
+            ProfilePicture()
+            ProfileContent()
+        }
+    }
+}
+
+@Composable
 fun ProfilePicture(){
+    Card(shape = CircleShape,
+    border = BorderStroke(width = 2.dp, color = Color.Green),
+        modifier = Modifier.padding(16.dp),
+        elevation = 4.dp
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription ="painter",
+            modifier = Modifier.size(72.dp))
+    }
 
 }
 
 @Composable
 fun ProfileContent(){
-
-}
-
-@Composable
-fun ProfileCard(){
-    Card(modifier = Modifier.fillMaxWidth(),
-    elevation = 8.dp) {
-        Row(modifier = Modifier.wrapContentSize()) {
-            ProfilePicture()
-            ProfileContent()
-        }
-    }
+    Text(text = "John do")
 }
 
 @Preview(showBackground = true)
